1e3f8f84091336d78b7ce178082e6fb17f94d104
rbays30 <lbays30@gmail.com>
2026-06-01 10:32:40 -0400

    Edited base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // For user/organization pages on GitHub Pages
   plugins: [vue()],
 })
