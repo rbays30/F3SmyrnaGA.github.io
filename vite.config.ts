e5fce70b07bd546a37defec1bbc51b4be218e310
rbays30 <lbays30@gmail.com>
2026-06-01 10:24:15 -0400

    Edited base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/F3SmyrnaGA.github.io/', // For user/organization pages on GitHub Pages
+  base: '/', // For user/organization pages on GitHub Pages
   plugins: [vue()],
 })
